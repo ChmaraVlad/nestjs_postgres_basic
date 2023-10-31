f1d810412d85feb0e0533cdce561edd868579a0d
Vlad Chmara <chmaravlad5@gmail.com>
2023-10-31 23:52:57 +0200

    updated app module and scripts added ENV config and env files for differebt configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,25 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    AuthModule,
-    UserModule,
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nest-course',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       models: [],
       autoLoadModels: true,
     }),
+    AuthModule,
+    UserModule,
   ],
 })
 export class AppModule {}
