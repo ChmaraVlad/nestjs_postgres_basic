a3d6b6d4d5324c577a19233449cb1610024f8a4d
Vlad Chmara <chmaravlad5@gmail.com>
2023-10-31 23:36:44 +0200

    postgresQl DB init comit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nest-course',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
