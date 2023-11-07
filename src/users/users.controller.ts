d80a67f6a1d7e3f45bf482e59b92c30863a9a8fe
Vlad Chmara <chmaravlad5@gmail.com>
2023-11-07 22:48:51 +0200

    added roles-decorator and roles-guard, updated users.contr

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { UsersService } from './users.service';
@@ -8,24 +16,41 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesdGuard } from 'src/auth/roles.guard';
 
 @ApiTags('Users')
 @Controller('/users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @ApiOperation({ summary: 'Creating new user' })
-  @ApiResponse({ status: 200, type: User })
-  @Post()
-  createUser(@Body() userDto: CreateUserDto) {
-    return this.usersService.createUser(userDto);
-  }
-
   @ApiOperation({ summary: 'Getting all users' })
   @ApiResponse({ status: 200, type: [User] })
   @UseGuards(JwtAuthGuard)
+  @Roles('USER', 'ADMIN')
+  @UseGuards(RolesdGuard)
   @Get()
   getAllUsers() {
     return this.usersService.getAllUsers();
+  }
+
+  @ApiOperation({ summary: 'Delete all users' })
+  @ApiResponse({ status: 200, type: [User] })
+  @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RolesdGuard)
+  @Delete()
+  deleteAllUsers() {
+    return this.usersService.deleteAllUsers();
+  }
+
+  @ApiOperation({ summary: 'Delete user  by id' })
+  @ApiResponse({ status: 200, type: User })
+  @UseGuards(JwtAuthGuard)
+  @Roles('ADMIN')
+  @UseGuards(RolesdGuard)
+  @Delete(':id')
+  deleteUserById(@Param('id') id: number) {
+    return this.usersService.deleteUserById(id);
   }
 }
