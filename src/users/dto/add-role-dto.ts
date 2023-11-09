94bcd8b92bed27151d168f5739868611046232c8
Vlad Chmara <chmaravlad5@gmail.com>
2023-11-09 18:35:39 +0200

    added types for validation for createRole dto

--- src/users/dto/add-role-dto.ts
+++ src/users/dto/add-role-dto.ts
@@ -1,4 +1,8 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class AddRoleDto {
+  @IsString({ message: 'Must be a string' })
   readonly userId: number;
+  @IsNumber({}, { message: 'Must be a number' })
   readonly value: string;
 }
